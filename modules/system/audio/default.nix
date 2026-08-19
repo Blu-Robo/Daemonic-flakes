98107b1f1ea904a5d4f5823b10e4bf3724b29619
blu-robo <brett-klenklen@proton.me>
2026-08-19 02:17:42 -0600

    added audio settings

---
+++ modules/system/audio/default.nix
@@ -0,0 +1,20 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.audio = { pkgs, lib, ... }: {
+    environment.systemPackages = with pkgs ; [
+      playerctl
+      pavucontrol
+      pulseaudioFull
+    ];
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+
+    nixpkgs.config.pulseaudio = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
+}

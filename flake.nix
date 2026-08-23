1c243072c4dc729e8227cb36691667d0cdfc243e
blu-robo <brett-klenklen@proton.me>
2026-08-23 16:57:49 -0600

    fixed some formatting inconsistencies and removed redundant noctalia flake input

--- flake.nix
+++ flake.nix
@@ -1,25 +1,20 @@
-{
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		flake-parts.url = "github:hercules-ci/flake-parts";
-		import-tree.url = "github:vic/import-tree";
-		wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+{ 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
 
-		nixos-hardware = {
-		  url = "github:NixOS/nixos-hardware/master";
-		  inputs.nixpkgs.follows = "nixpkgs";
-		};
-		zen-browser = {
-			url = "github:youwen5/zen-browser-flake";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	#	noctalia = {
-	#		url = "github:noctalia-dev/noctalia";
-	#		inputs.nixpkgs.follows = "nixpkgs";
-	#	};
-	};
-
-	outputs = inputs: inputs.flake-parts.lib.mkFlake
-		{inherit inputs;}
-		(inputs.import-tree ./modules);
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake
+    {inherit inputs;}
+    (inputs.import-tree ./modules);
 }

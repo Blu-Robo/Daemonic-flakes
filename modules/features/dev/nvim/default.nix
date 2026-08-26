03ff6bf6511989b40646e7477c94cc3691f65ebf
blu-robo <brett-klenklen@proton.me>
2026-08-25 23:39:57 -0600

    Added some basic neovim theming and plugins

--- modules/features/dev/nvim/default.nix
+++ modules/features/dev/nvim/default.nix
@@ -1,9 +1,41 @@
 { self, inputs, ... }: {
   flake.nixosModules.nvim = { pkgs, lib, ... }: {
     imports = [ inputs.nvf.nixosModules.default ];
+
     programs.nvf = {
       enable = true;
-      settings = {
+      settings.vim = {
+        theme = {
+          enable = true;
+          name = "catppuccin";
+          style = "mocha";
+          transparent = true;
+        };
+
+        filetree.neo-tree = {
+          enable = true;
+          setupOpts = {
+            enable_cursor_hijack = true;
+            enable_git_status = true;
+            filesystem.filtered_items.visible = true;
+          };
+        };
+        
+        statusline.lualine = {
+          enable = true;
+          icons.enable = true;
+        };
+        
+        treesitter = {
+          enable = true;
+        };
+        
+        languages = {
+          nix.enable = true;
+          clang.enable = true;
+        };
+        
+        telescope.enable = true;
       };
     };
   };

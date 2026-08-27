d9e14440306eebbdb8745ac9fc31fd8edaebce38
blu-robo <brett-klenklen@proton.me>
2026-08-27 10:17:45 -0600

    added printing access

--- modules/features/school/default.nix
+++ modules/features/school/default.nix
@@ -3,5 +3,13 @@
     environment.systemPackages = with pkgs; [
       obsidian
     ];
+    services = {
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+      };
+      printing.enable = true;
+    };
   };
 }

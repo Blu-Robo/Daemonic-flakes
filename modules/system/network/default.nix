ca0d7e73cd2e5edde3c78920152dce742705d11b
blu-robo <brett-klenklen@proton.me>
2026-08-28 10:25:42 -0600

    Added protonvpn support

--- modules/system/network/default.nix
+++ modules/system/network/default.nix
@@ -10,11 +10,18 @@
         "1.1.1.1"
 	"8.8.8.8"
       ];
-      firewall.enable = false;
+      firewall = {
+        enable = false;
+        checkReversePath = false;
+      };
     };
     services.unbound = {
       enable = true;
     };
+    environment.systemPackages = with pkgs; [
+      wireguard-tools
+      proton-vpn
+    ];
     systemd.services.NetworkManager-wait-online.enable = false;
   };
 }
